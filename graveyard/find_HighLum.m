2ee43d4c04c065abcaf1fef988b1927eab8ca6cc
Adam Miller <52980118+thisisampm@users.noreply.github.com>
2022-09-15 14:13:32 -0400

    Delete graveyard directory

--- graveyard/find_HighLum.m
+++
@@ -1,9 +0,0 @@
-function HighLum_idx = find_HighLum(lum_mean, videoObject, min_sec)
-% find LED-on instances in a minimic imaging file
-
-% correct brightness
-lum_bounds = [50 64];
-lum_bounds_idx = lum_mean > lum_bounds(1) & lum_mean < lum_bounds(2);
-
-% correct duration
-
